0c44a2036c9bdc3d1c910bb3d7365e3408712207
Mahdi <Mahdikgz7@gmail.com>
2025-07-14 12:45:42 +0330

    chore: header done

--- src/modules/dashboard/layout/Header.tsx
+++ src/modules/dashboard/layout/Header.tsx
@@ -1,9 +1,24 @@
+"use client";
 import React from "react";
+import { RootState } from "@/src/redux/store";
+import { useSelector } from "react-redux";
+import ProfileIcon from "@/src/icons/ProfileIcon";
+import NotificationsIcon from "@/src/icons/NotificationsIcon";
 
 function DashboardHeader() {
+  const { user } = useSelector((state: RootState) => state.auth);
   return (
-    <div className="w-full flex items-center bg-base-100 rounded-lg text-white h-16 px-6 py-3">
+    <div className="w-full flex items-center justify-between bg-base-100 rounded-lg text-white h-16 px-6 py-3 pl-16">
       <h1 className="title-graident font-bold text-xl">حساب کاربری</h1>
+      <div className="flex items-center gap-x-6">
+        <button className="btn btn-circle">
+          <NotificationsIcon />
+        </button>
+        <div className="flex items-center justify-center gap-x-2">
+          <ProfileIcon />
+          <span className="font-normal text-base">{user?.username}</span>
+        </div>
+      </div>
     </div>
   );
 }
